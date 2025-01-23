456460df38963530821245c492bbd0ba01b96850
waleedkhalid777 <160757793+waleedkhalid777@users.noreply.github.com>
2025-01-23 18:10:07 +0500

    sanityclient.ts

--- src/sanity/lib/sanityclient.ts
+++ src/sanity/lib/sanityclient.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 const client = createClient({
   projectId: "6spgz8iy", // Replace with your actual Sanity project ID
   dataset: "production",        // Replace with your dataset name (default: "production")
-  apiVersion: "2021-08-31",     // Use the latest API version or specify a compatible one
+  apiVersion: "2025-01-13",     // Use the latest API version or specify a compatible one
   useCdn: true,                 // `true` for faster reads via CDN; `false` for real-time data
   token: "skbk9Jfk7bzdAUdF4sXaxeaH7vylOipGFWNLHJaMeFYNkViLJamamgs4kCycpfuPftIWV2w6rNYvaCi4Fcr5cVTAbpm6kLkOnTslK7oQyRZ61gC6ykdEtobSMncA9zq0HaNvTZDoN2o1kEApZNdly7j6YhvmJwcS3u3eDpvkASSgITrAB344",   // Optional: Add this if you need to access private data
 });
