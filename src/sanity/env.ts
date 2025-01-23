543182b782b64a11939ee0bf3b7630c8c0216b8a
waleedkhalid777 <160757793+waleedkhalid777@users.noreply.github.com>
2025-01-23 18:11:02 +0500

    env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,7 +2,7 @@
 
 
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2021-08-31'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-13'
 
 export const dataset = assertValue(
   "production",
